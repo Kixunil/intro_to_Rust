60596db902bcdca96e9fd3c48118df87ed561bae
Martin Habovštiak <martin.habovstiak@gmail.com>
2017-05-05 22:25:48 +0200

    Added variables_example.

---
+++ variables_example/README.md
@@ -0,0 +1,6 @@
+Variables example
+=================
+
+To show how variables are created in Rust, let's write a program that computes the answer to Life, Universe and Everything.
+
+Open the source code and DON'T PANIC!
---
+++ variables_example/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let the_answer = 42;
+    println!("The answer is: {}", the_answer);
+}
+
+// So, C programmers be like: "Why is integer called 'let'?"
+// Javascript programmers: "Huh, dynamic typing. I know that!"
+// Haskell programmers: "Yeah, full-program inference!"
+//
+// You are all wrong.
+//
+// Rust uses function-local inference to avoid writing boilerplate. Function signatures are still
+// explicit because they serve as a documentation, a backward-compatibility promise in libraries
+// they make type compilation faster and provide more precise error messages.
+//
+// Rust doesn't allow variables to change types. They can be shadowed, though! That means if you
+// ever introduce new variable with same name, you lose access to the previous one. The new one can
+// have different type.
+//
+// So what does `println!("The answer is: {}", the_answer)` mean?
+// The curly braces inside zeroth macro argument tell Rust that a displayable value should go
+// there. The first argument is our variable. The variable is actually of type u32 and that type is
+// displayable.
+//
+// Ok, enough talking, let's compute the Question. See ownership_example.
